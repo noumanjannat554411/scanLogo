583a50bd79321d33c803b4839764911ab5d219df
supervet <90903450+supervetcode@users.noreply.github.com>
2026-02-10 18:50:46 +0500

    AR functionality updated and working

--- android/app/src/main/java/com/scanlogo/ARViewerModule.kt
+++ android/app/src/main/java/com/scanlogo/ARViewerModule.kt
@@ -56,12 +56,12 @@ class ARViewerModule(reactContext: ReactApplicationContext) : ReactContextBaseJa
 
             val testIntent = Intent(Intent.ACTION_VIEW)
             testIntent.data = Uri.parse("https://arvr.google.com/scene-viewer/1.0")
-            testIntent.setPackage("com.google.android.googlequicksearchbox")
+            testIntent.setPackage("com.google.ar.core")
             
             val isSupported = testIntent.resolveActivity(activity.packageManager) != null
-            promise.resolve("isSupported khan" + isSupported)
+            promise.resolve(isSupported)
         } catch (e: Exception) {
-            promise.resolve(" khan")
+            promise.resolve(false)
         }
     }
 }
